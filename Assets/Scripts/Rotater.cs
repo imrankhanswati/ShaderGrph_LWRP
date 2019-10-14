c3acb6a80f36cba4e4b427d5aa2223a756da288e
imrankhanswati <imrankhanswati315@gmail.com>
2019-10-14 16:23:40 +0500

    Init Shaders
    1. Hologram
    2. Dessolve
    3. Glowing Grid
    4. Mesh destortion
    5. Glowing strip

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    [SerializeField] private Vector3 rotation;
+
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime, Space.Self);
+    }
+}
